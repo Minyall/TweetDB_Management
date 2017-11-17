990aec25d9c093642dfd98a826f25550a15ab5ee
James Allen-Robertson <minyall@gmail.com>
2017-11-17 08:34:57 +0000

    Switched to aggregate query for MongoDB to speed up retrieval. Also switched to enumerate for the counter for efficiency. Aggregate meant lost the capacity to know total record count so removed for now.

--- retweet_extract.py
+++ retweet_extract.py
@@ -4,15 +4,14 @@ import pytz
 from SETTINGS import database, collection
 connection = pymongo.MongoClient('localhost', 27017)
 db = connection[database][collection]
-cursor = db.find({'retweeted_status':{'$exists':True}}, no_cursor_timeout=True)
-total = cursor.count()
-counter = 0
-for x in cursor:
-    if counter % 10000 == 0:
-        print('Extracted {} of {} Retweets'.format(counter,total))
+cursor = db.aggregate(pipeline=[{'$match':{'retweeted_status':{'$exists':1}}}])
+# total = cursor.count()
+
+for i, x in enumerate(cursor):
+    if i % 10000 == 0:
+        print('Extracted {} Retweets'.format(i))
     # print(type(x['retweeted_status']))
     # print(x['retweeted_status'])
     connection[database][collection].update_one({'id': x['id']}, {'$setOnInsert':x['retweeted_status']}, upsert=True)
     connection[database][collection].update_one({'id': x['id']},
     {'$max':{'retweet_count':x['retweeted_status']['retweet_count']}})
-    counter += 1
